484e39c68c0547ebd3222ebd77dc427df3bd865c
firescrypt <83104991+firescrypt@users.noreply.github.com>
2021-06-14 19:02:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('heroku.html').pipe(res)
+  fs.createReadStream('index.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 3000)
